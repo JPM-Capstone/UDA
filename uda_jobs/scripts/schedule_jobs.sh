105791dbf10090dbc2fd69c8667c8fe42b44d53e
ShubhamKaushal15 <shubham.a.kaushal@gmail.com>
2023-04-13 00:48:44 -0400

    5 jobs ready

---
+++ uda_jobs/scripts/schedule_jobs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sbatch ./uda_jobs/sbatch/job1.sbatch
+sbatch ./uda_jobs/sbatch/job2.sbatch
+sbatch ./uda_jobs/sbatch/job3.sbatch
+sbatch ./uda_jobs/sbatch/job4.sbatch
+sbatch ./uda_jobs/sbatch/job5.sbatch
